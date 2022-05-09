bd053a9dab3b76d44b465c9b45640372bc6cc91d
Ataize Feitosa <isa.aragao@hotmail.com>
2022-05-09 11:09:29 -0300

    Adicionando o produto clicado e sua descrição na página Ver Produto

--- js/verProduto.js
+++ js/verProduto.js
@@ -9,5 +9,10 @@ window.onload = ()=>{
         return item.id === id
     })
     produto.childNodes[1].src = produtoDestaque[0].imagem
-   
+
+    let informacoesProduto = produto.childNodes[3];
+    informacoesProduto.childNodes[1].textContent = produtoDestaque[0].nome;
+    informacoesProduto.childNodes[2].textContent = produtoDestaque[0].preco;
+    informacoesProduto.childNodes[3].textContent = produtoDestaque[0].descricao;
+
 }
